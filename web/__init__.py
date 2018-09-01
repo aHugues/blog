125c4b94e3f0f2784457103efa1a18e26a93a226
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2018-09-01 20:35:47 +0200

    Put database config in config file

--- web/__init__.py
+++ web/__init__.py
@@ -3,14 +3,22 @@ from .views import main_views
 from .views import blog_views
 from .views import admin_views
 from .models import db
+import configparser
 
-def create_app(debug=False):
+config = configparser.ConfigParser()
+
+def create_app(debug=False, production=False):
+    if production:
+        config.read('web/config/config.prod.ini')
+    else:
+        config.read('web/config/config.dev.ini')
+    print(config)
     app = Flask(__name__)
     app.debug = debug
     app.register_blueprint(main_views)
     app.register_blueprint(blog_views)
     app.register_blueprint(admin_views)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://blog:password@localhost/blog'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE']['SQLAlchemy_uri']
     db.init_app(app)
     return app
