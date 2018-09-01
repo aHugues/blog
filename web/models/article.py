b329e24d1d3fe33561e6f82b1bc852843a163d54
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2018-09-01 20:36:00 +0200

    Update articles model

--- web/models/article.py
+++ web/models/article.py
@@ -6,9 +6,13 @@ class Article(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
+    content = db.Column(db.Text())
+    date = db.Column(db.Date())
 
-    def __init__(self, title):
+    def __init__(self, title, content, date):
         self.title = title
+        self.content = content
+        self.date = date
     
     def __repr__(self):
         return '<id {}'.format(self.id)
