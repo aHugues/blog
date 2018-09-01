841a2b008445dc096f08cbb1f42fc5f50c370ba3
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2018-09-01 12:22:25 +0200

    Add base admin page

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .views import main_views
 from .views import blog_views
+from .views import admin_views
 
 def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
     app.register_blueprint(main_views)
     app.register_blueprint(blog_views)
+    app.register_blueprint(admin_views)
     return app
