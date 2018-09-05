c7fbed004121b05b02eb57eaa51b96dcc6eb00c4
Aurélien Hugues <aurelien.hugues.59@gmail.com>
2018-09-05 20:15:58 +0200

    Add image to articles model

--- web/models/article.py
+++ web/models/article.py
@@ -8,11 +8,13 @@ class Article(db.Model):
     title = db.Column(db.String(128))
     content = db.Column(db.Text())
     date = db.Column(db.Date())
+    image = db.Column(db.String(64))
 
-    def __init__(self, title, content, date):
+    def __init__(self, title, content, date, image):
         self.title = title
         self.content = content
         self.date = date
+        self.image = image
     
     def __repr__(self):
         return '<id {}>'.format(self.id)
